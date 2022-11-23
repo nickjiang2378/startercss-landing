6aef0758938b7edd29056bed69b54045253e27bc
Nick Jiang <nickj@berkeley.edu>
2022-11-22 17:26:31 -0800

    Added demo video and installation link

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,16 @@ export default function Home() {
           Rapidly style frontends with <span className={styles.gradientHighlight}>StarterCSS</span>
         </h1>
 
+        <div style={{ position: "relative", paddingBottom: "54.05405405405405%", height: 0}}>
+          <iframe src="https://www.loom.com/embed/a7151d17b244479c8a1fe0a3492afec6" 
+              frameBorder="0" 
+              webkitAllowFullscreen 
+              mozAllowFullscreen 
+              allowFullScreen 
+              style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}}>
+          </iframe>
+        </div>
+
         <p className={styles.description}>
           StarterCSS is a chrome extension for web developers 
           to effortlessly implement CSS layouts, effects, and designs without parsing through documentation. 
@@ -23,41 +33,18 @@ export default function Home() {
         
         <p className={styles.description}>
           Picking up CSS can be challenging, and StarterCSS saves you the 
-          time of pouring through tutorials with an intuitive dashboard to visualize and add common CSS features. 
+          time of pouring through tutorials with an intuitive dashboard to visualize and customize common CSS features. 
           Make your changes at a few clicks, and understand why they worked.
         </p>
 
-        {/*<div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <p className={styles.description}>
+          To try it out, head over to the <a target="_blank" className={styles.link} href="https://chrome.google.com/webstore/detail/iafddfmhieikpdmmakjedpcconjgcgga">Web store</a>. 
+        </p>
+       
+        <p className={styles.description}>
+          I'm happy to hear your thoughts! Feel free to DM me on <a target="_blank" className={styles.link} href="https://twitter.com/nickhjiang">Twitter</a> or message me at +1 9258581652.
+        </p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>*/}
       </main>
     </div>
   )
